a2f8103ded4bb3e0f4b842b5c818efc6a6142432
Matt Carroll <me@mattcarroll.name>
2018-04-17 00:24:20 -0700

    Extracted app bar to app_bar.dart

---
+++ lib/forecast/app_bar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ForecastAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new AppBar(
+      centerTitle: false,
+      backgroundColor: Colors.transparent,
+      elevation: 0.0,
+      title: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Text(
+            'Thursday, August 29',
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+            ),
+          ),
+          new Text(
+              'Sacramento',
+              style: new TextStyle(
+                color: Colors.white,
+                fontSize: 30.0,
+              )
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        new IconButton(
+            icon: new Icon(
+              Icons.arrow_forward_ios,
+              color: Colors.white,
+              size: 35.0,
+            ),
+            onPressed: () {
+              // TODO:
+            }
+        ),
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:weather/forecast/app_bar.dart';
 import 'package:weather/forecast/background/background_with_rings.dart';
 
 void main() {
@@ -45,42 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
             top: 0.0,
             left: 0.0,
             right: 0.0,
-            child: new AppBar(
-              centerTitle: false,
-              backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              title: new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(
-                    'Thursday, August 29',
-                    style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 16.0,
-                    ),
-                  ),
-                  new Text(
-                    'Sacramento',
-                    style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 30.0,
-                    )
-                  ),
-                ],
-              ),
-              actions: <Widget>[
-                new IconButton(
-                  icon: new Icon(
-                    Icons.arrow_forward_ios,
-                    color: Colors.white,
-                    size: 35.0,
-                  ),
-                  onPressed: () {
-                    // TODO:
-                  }
-                ),
-              ],
-            ),
+            child: new ForecastAppBar(),
           ),
         ],
       ),
