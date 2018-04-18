79e5e74149a43317a0d486097203b73c70ac3510
Matt Carroll <me@mattcarroll.name>
2018-04-17 18:09:02 -0700

    Implemented static week drawer on top of background.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:weather/forecast/app_bar.dart';
@@ -29,6 +31,36 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  final week = [
+    'Tuesday\nAugust 27',
+    'Wednesday\nAugust 28',
+    'Thursday\nAugust 29',
+    'Friday\nAugust 30',
+    'Saturday\nAugust 31',
+    'Sunday\nAugust 1',
+    'Monday\nAugust 2',
+  ];
+
+  List<Widget> _buildDayButtons() {
+    return week.map((String title) {
+      return new Expanded(
+        child: new GestureDetector(
+          onTap: () {
+            // TODO:
+          },
+          child: new Text(
+            title,
+            textAlign: TextAlign.center,
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 14.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -48,9 +80,49 @@ class _MyHomePageState extends State<MyHomePage> {
             right: 0.0,
             child: new ForecastAppBar(),
           ),
+
+          // Right Drawer
+          new ClipRect(
+            clipper: new RectClipper(),
+            child: new BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+              child: new Container(
+                width: 125.0,
+                height: double.infinity,
+                color: const Color(0xAA234060),
+                child: new Padding(
+                  padding: const EdgeInsets.only(top: 16.0),
+                  child: new Column(
+                    children: <Widget>[
+                      new Expanded(
+                        child: new Icon(
+                          Icons.refresh,
+                          color: Colors.white,
+                          size: 40.0,
+                        ),
+                      ),
+                    ]
+                    ..addAll(_buildDayButtons()),
+                  )
+                )
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 }
 
+class RectClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return new Rect.fromLTWH(0.0, 0.0, size.width, size.height);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+
+}
