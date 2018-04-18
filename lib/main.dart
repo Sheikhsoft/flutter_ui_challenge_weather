c047ec45c4e80640627d41d80520fe5ba933503b
Matt Carroll <me@mattcarroll.name>
2018-04-17 23:10:20 -0700

    Implemented background with rings.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather/forecast/background/background_with_rings.dart';
 
 void main() => runApp(new MyApp());
 
@@ -25,7 +26,12 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: new Center(),
+      body: new Stack(
+        children: <Widget>[
+          new BackgroundWithRings(),
+        ],
+      ),
     );
   }
 }
+
