fcc142ec4b001a286142902791407f1093759c71
Matt Carroll <me@mattcarroll.name>
2018-04-17 18:11:23 -0700

    Extracted static week drawer to week_drawer.dart

---
+++ lib/forecast/week_drawer.dart
@@ -0,0 +1,79 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class WeekDrawer extends StatelessWidget {
+
+  final week = [
+    'Tuesday\nAugust 27',
+    'Wednesday\nAugust 28',
+    'Thursday\nAugust 29',
+    'Friday\nAugust 30',
+    'Saturday\nAugust 31',
+    'Sunday\nAugust 1',
+    'Monday\nAugust 2',
+  ];
+
+  List<Widget> _buildDayButtons() {
+    return week.map((String title) {
+      return new Expanded(
+        child: new GestureDetector(
+          onTap: () {
+            // TODO:
+          },
+          child: new Text(
+            title,
+            textAlign: TextAlign.center,
+            style: new TextStyle(
+              color: Colors.white,
+              fontSize: 14.0,
+            ),
+          ),
+        ),
+      );
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new ClipRect(
+      clipper: new RectClipper(),
+      child: new BackdropFilter(
+        filter: new ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+        child: new Container(
+            width: 125.0,
+            height: double.infinity,
+            color: const Color(0xAA234060),
+            child: new Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: new Column(
+                  children: <Widget>[
+                    new Expanded(
+                      child: new Icon(
+                        Icons.refresh,
+                        color: Colors.white,
+                        size: 40.0,
+                      ),
+                    ),
+                  ]
+                    ..addAll(_buildDayButtons()),
+                )
+            )
+        ),
+      ),
+    );
+  }
+}
+
+class RectClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return new Rect.fromLTWH(0.0, 0.0, size.width, size.height);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:weather/forecast/app_bar.dart';
 import 'package:weather/forecast/background/background_with_rings.dart';
+import 'package:weather/forecast/week_drawer.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
@@ -31,36 +32,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  final week = [
-    'Tuesday\nAugust 27',
-    'Wednesday\nAugust 28',
-    'Thursday\nAugust 29',
-    'Friday\nAugust 30',
-    'Saturday\nAugust 31',
-    'Sunday\nAugust 1',
-    'Monday\nAugust 2',
-  ];
-
-  List<Widget> _buildDayButtons() {
-    return week.map((String title) {
-      return new Expanded(
-        child: new GestureDetector(
-          onTap: () {
-            // TODO:
-          },
-          child: new Text(
-            title,
-            textAlign: TextAlign.center,
-            style: new TextStyle(
-              color: Colors.white,
-              fontSize: 14.0,
-            ),
-          ),
-        ),
-      );
-    }).toList();
-  }
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -82,47 +53,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
 
           // Right Drawer
-          new ClipRect(
-            clipper: new RectClipper(),
-            child: new BackdropFilter(
-              filter: new ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-              child: new Container(
-                width: 125.0,
-                height: double.infinity,
-                color: const Color(0xAA234060),
-                child: new Padding(
-                  padding: const EdgeInsets.only(top: 16.0),
-                  child: new Column(
-                    children: <Widget>[
-                      new Expanded(
-                        child: new Icon(
-                          Icons.refresh,
-                          color: Colors.white,
-                          size: 40.0,
-                        ),
-                      ),
-                    ]
-                    ..addAll(_buildDayButtons()),
-                  )
-                )
-              ),
-            ),
-          )
+          new WeekDrawer(),
         ],
       ),
     );
   }
-}
-
-class RectClipper extends CustomClipper<Rect> {
-  @override
-  Rect getClip(Size size) {
-    return new Rect.fromLTWH(0.0, 0.0, size.width, size.height);
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-    return true;
-  }
-
 }
